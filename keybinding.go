9cdd627c541dba801506d1e11bf6fccf2520b807
Antoine Broyelle <antoine.broyelle@grenoble-inp.fr>
2016-05-20 09:34:34 +0200

    Navigation primitives
    [end]: go to end of line
    [home]: go to beginning of the line
    [pgup]: go one view upper
    [pgdown]: go one view lower

--- keybinding.go
+++ keybinding.go
@@ -12,11 +12,22 @@ func initKeybindings(g *gocui.Gui) error {
 	if err := g.SetKeybinding("", gocui.KeyCtrlT, gocui.ModNone, currTopViewHandler("cmdline")); err != nil {
 		return err
 	}
-
 	if err := g.SetKeybinding("cmdline", gocui.KeyCtrlT, gocui.ModNone, currTopViewHandler("main")); err != nil {
 		return err
 	}
 
+	if err := g.SetKeybinding("", gocui.KeyHome, gocui.ModNone, cursorHome); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("", gocui.KeyEnd, gocui.ModNone, cursorEnd); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("", gocui.KeyPgup, gocui.ModNone, goPgUp); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("", gocui.KeyPgdn, gocui.ModNone, goPgDown); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -32,4 +43,53 @@ func currTopViewHandler(name string) gocui.KeybindingHandler {
 		_, err := g.SetViewOnTop(name)
 		return err
 	}
+}
+
+func cursorHome(g *gocui.Gui, v *gocui.View) error {
+	if v != nil {
+		cx, _ := v.Cursor()
+		v.MoveCursor(-cx, 0, true)
+	}
+	return nil
+}
+
+func cursorEnd(g *gocui.Gui, v *gocui.View) error {
+	if v != nil {
+		cx, cy := v.Cursor()
+		l, _ := v.Line(cy)
+		v.MoveCursor(len(l)-cx, 0, true)
+	}
+	return nil
+}
+
+func goPgUp(g *gocui.Gui, v *gocui.View) error {
+	if v != nil {
+		ox, oy := v.Origin()
+		_, y := v.Size()
+		yOffset := oy - y
+		if yOffset < 0 {
+			v.SetOrigin(ox, 0)
+			if oy == 0 {
+				_, cy := v.Cursor()
+				v.MoveCursor(0, -cy, false)
+			}
+		} else {
+			v.SetOrigin(ox, yOffset)
+			v.MoveCursor(0, -yOffset, false)
+		}
+	}
+	return nil
+}
+
+func goPgDown(g *gocui.Gui, v *gocui.View) error {
+	if v != nil {
+		ox, oy := v.Origin()
+		_, y := v.Size()
+		err := v.SetOrigin(ox, oy+y)
+		if err != nil {
+			return err
+		}
+		v.MoveCursor(0, 0, false)
+	}
+	return nil
 }
