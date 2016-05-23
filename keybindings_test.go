aaabc208d1ec0c95641f9fdbd7c7884b1434f66d
Valentin Matton <valentin.matton@ensimag.grenoble-inp.fr>
2016-05-23 16:59:18 +0200

    Added tests for various keybindings handlers

--- keybindings_test.go
+++ keybindings_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/stretto-editor/gocui"
+	"strings"
 	"testing"
 )
 
@@ -69,4 +70,66 @@ func TestSwitchMode(t *testing.T) {
 	if m := g.CurrentMode(); m.Name() != "testB" {
 		t.Error("Wrong current mode")
 	}
+}
+
+func TestPageDownUp(t *testing.T) {
+	g := gocui.NewGui()
+	g.Init()
+	defer g.Close()
+
+	maxX, maxY := g.Size()
+	v, _ := g.SetView("test", 0, 0, maxX, maxY)
+	g.SetCurrentView("test")
+	for i := 0; i < 1000; i++ {
+		fmt.Fprintf(v, "%d\n", i)
+	}
+
+	goPgDown(g, v)
+	goPgDown(g, v)
+
+	if fl := strings.Split(v.ViewBuffer(), "\n"); fl[0] != string(2*maxY-1) {
+		t.Errorf("Found line no %s instead of line no %d", fl[0], 2*maxY-1)
+	}
+
+	goPgUp(g, v)
+	if fl := strings.Split(v.ViewBuffer(), "\n"); fl[0] != string(maxY-1) {
+		t.Errorf("Found line no %s instead of line no %d", fl[0], 2*maxY-1)
+	}
+}
+
+func TestCurrTopViewHandler(t *testing.T) {
+
+	g := gocui.NewGui()
+	g.Init()
+	defer g.Close()
+
+	maxX, maxY := g.Size()
+	vA, _ := g.SetView("testA", 0, 0, maxX, maxY)
+	fmt.Fprintf(vA, "foo")
+	vB, _ := g.SetView("testB", 0, 0, maxX, maxY)
+	fmt.Fprintf(vB, "bar")
+	g.SetCurrentView("testA")
+
+	f := currTopViewHandler("testB")
+	f(g, vA)
+	if g.CurrentView() != vB {
+		t.Error("testB was expected to be the current view")
+	}
+	if g.CurrentView().ViewBuffer() != vB.ViewBuffer() {
+		t.Error("testB was expected to be on top")
+	}
+}
+
+func TestInitKeybindings(t *testing.T) {
+	g := gocui.NewGui()
+	g.Init()
+	defer g.Close()
+
+	initModes(g)
+	g.SetLayout(layout)
+
+	if err := initKeybindings(g); err != nil {
+		t.Error("bad keybindings initialization")
+	}
+
 }
