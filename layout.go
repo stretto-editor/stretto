3c441d5610050557049b15e37e56316479f4c410
Antoine Broyelle <antoine.broyelle@grenoble-inp.fr>
2016-06-06 16:58:59 +0200

    Use one function per view to update geometry
    To update the geometry of the view, you need to create a fonction which
    respect the updateView prototype. Than place this fonction into the up field
    to the related viewInfo.
    
    By using such architecture, we allow dynamic creations of view by feeding
    requiredViewsInfo on the run.

--- layout.go
+++ layout.go
@@ -8,46 +8,96 @@ import (
 	"github.com/stretto-editor/gocui"
 )
 
-var requiredViewsInfo map[string]*struct {
+// updateView is a fonction type used for updating view
+type updateView func(int, int)
+
+type viewInfo struct {
 	x, y, w, h int
-	c          string // father container
-	t          string // Title
-	e          bool   // Editable
-	f          string // Footer
-	hi         bool   // Hidden
-	wr         bool   // Wrap
+	c          string     // father container
+	t          string     // Title
+	e          bool       // Editable
+	f          string     // Footer
+	hi         bool       // Hidden
+	wr         bool       // Wrap
+	up         updateView // update
 }
 
-func initRequiredViewsInfo(g *gocui.Gui) {
+var requiredViewsInfo map[string]*viewInfo
 
-	requiredViewsInfo = map[string]*struct {
-		x, y, w, h int
-		c          string // father container
-		t          string // Title
-		e          bool   // Editable
-		f          string // Footer
-		hi         bool   // Hidden
-		wr         bool   // Wrap
-	}{
-		"file": {t: "",
-			c: "main",
-			e: true},
-		"cmdline": {t: "Commandline",
-			c: "editable",
-			e: true},
-		"inputline": {t: "Inputline for interactive actions",
-			c:  "editable",
-			e:  true,
-			hi: true},
-		"infoline": {e: true,
-			f: "INFO"},
-		"error": {t: "Error :",
-			e:  true,
-			hi: true,
-			wr: true},
+var infoHeight = 2
+
+func initRequiredViewsInfo(g *gocui.Gui) {
+	updateFileGeom := func(maxX, maxY int) {
+		f, _ := requiredViewsInfo["file"]
+		f.w = maxX + 1
+		f.h = maxY - 1 - infoHeight
+		f.x = -1
+		f.y = 0
+	}
+	updateCmdlineGeom := func(maxX, maxY int) {
+		c, _ := requiredViewsInfo["cmdline"]
+		c.w = 30
+		c.h = 2
+		c.x = (maxX - c.w) / 2
+		c.y = maxY - c.h - 10
+	}
+	updateInputlineGeom := func(maxX, maxY int) {
+		inp, _ := requiredViewsInfo["inputline"]
+		inp.w = maxX * 80 / 100
+		inp.h = 2
+		inp.x = (maxX - inp.w) / 2
+		inp.y = maxY - inp.h - 5
+	}
+	updateInfolineGeom := func(maxX, maxY int) {
+		inf, _ := requiredViewsInfo["infoline"]
+		inf.w = maxX - 1
+		inf.h = infoHeight
+		inf.x = (maxX - inf.w) / 2
+		inf.y = maxY - inf.h - 1
+	}
+	updateErrorViewGeom := func(maxX, maxY int) {
+		e, _ := requiredViewsInfo["error"]
+		e.w = maxX - 1
+		e.h = 3
+		e.x = (maxX - e.w) / 2
+		e.y = maxY - infoHeight - e.h - 1
 	}
 
-	setDefaultGeometry(g.Size())
+	requiredViewsInfo = map[string]*viewInfo{
+		"file": {
+			t:  "",
+			c:  "main",
+			e:  true,
+			up: updateFileGeom,
+		},
+		"cmdline": {
+			t:  "Commandline",
+			c:  "editable",
+			e:  true,
+			up: updateCmdlineGeom,
+		},
+		"inputline": {
+			t:  "Inputline for interactive actions",
+			c:  "editable",
+			e:  true,
+			hi: true,
+			up: updateInputlineGeom,
+		},
+		"infoline": {
+			e:  true,
+			f:  "INFO",
+			up: updateInfolineGeom,
+		},
+		"error": {
+			t:  "Error :",
+			e:  true,
+			hi: true,
+			wr: true,
+			up: updateErrorViewGeom,
+		},
+	}
+
+	updateGeometry(g.Size())
 }
 
 func initTreeView(g *gocui.Gui) {
@@ -55,42 +105,14 @@ func initTreeView(g *gocui.Gui) {
 	g.SetViewNode("main", "editable", 0, 0, 10, 10)
 }
 
-func setDefaultGeometry(maxX, maxY int) {
-	infoHeight := 2
-	// default geometry
-	f, _ := requiredViewsInfo["file"]
-	f.w = maxX + 1
-	f.h = maxY - 1 - infoHeight
-	f.x = -1
-	f.y = 0
-
-	c, _ := requiredViewsInfo["cmdline"]
-	c.w = 30
-	c.h = 2
-	c.x = (maxX - c.w) / 2
-	c.y = maxY - c.h - 10
-
-	inp, _ := requiredViewsInfo["inputline"]
-	inp.w = maxX * 80 / 100
-	inp.h = 2
-	inp.x = (maxX - inp.w) / 2
-	inp.y = maxY - inp.h - 5
-
-	inf, _ := requiredViewsInfo["infoline"]
-	inf.w = maxX - 1
-	inf.h = infoHeight
-	inf.x = (maxX - inf.w) / 2
-	inf.y = maxY - inf.h - 1
-
-	e, _ := requiredViewsInfo["error"]
-	e.w = maxX - 1
-	e.h = 3
-	e.x = (maxX - e.w) / 2
-	e.y = maxY - inf.h - e.h - 1
+func updateGeometry(maxX, maxY int) {
+	for _, vi := range requiredViewsInfo {
+		vi.up(maxX, maxY)
+	}
 }
 
 func updateAllLayout(g *gocui.Gui) {
-	setDefaultGeometry(g.Size())
+	updateGeometry(g.Size())
 
 	if v, _ := g.View("error"); !v.Hidden {
 		m, _ := requiredViewsInfo["file"]
